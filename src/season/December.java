c3a285c3bfc9f9b30352b0d9c3aa3ad2e5b5e2c0
Gulayim <gokombayeva@bk.ru>
2024-01-24 22:31:06 +0600

    scrum-456/feature- december

---
+++ src/season/December.java
@@ -0,0 +1,8 @@
+package season;
+
+public class December {
+    private String holidays;
+    public static void main(String[] args) {
+        System.out.println("What holidays are there in December?");
+    }
+}
