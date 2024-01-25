523838e7dfdad68e883f507679af7c6846c04c48
Gulayim <gokombayeva@bk.ru>
2024-01-25 14:30:16 +0600

    fearIphone 14

--- src/C/C1.java
+++
@@ -1,7 +0,0 @@
-package C;
-
-public class C1 {
-    public static void main(String[] args) {
-        System.out.println("Hello Word");
-    }
-}
--- src/C/C2.java
+++
@@ -1,7 +0,0 @@
-package C;
-
-public class C2 {
-    public static void main(String[] args) {
-
-    }
-}
